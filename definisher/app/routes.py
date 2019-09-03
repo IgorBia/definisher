3cca8ff5434875fb06f3751502b3734d074ab754
GummyBear <dawid1siekanski@gmail.com>
2019-09-03 21:52:30 +0200

    abcd

--- definisher/app/routes.py
+++ definisher/app/routes.py
@@ -3,7 +3,9 @@ from app import script
 from flask import render_template
 
 @app.route('/')
+def aaa():
+    return render_template('state1.html')
 @app.route('/index')
 def index():
     content = script.do().decode().split("SplittingSentense")
-    return render_template('index.html', title=content[0], content = content[1])
+    return render_template('base.html', title=content[0], content = content[1])
