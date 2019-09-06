6b69c18a8da208563e764ab425fcc38612e82b62
Bialaz <igor.bialas@gmail.com>
2019-09-06 22:47:23 +0200

    jd

--- definisher/app/routes.py
+++ definisher/app/routes.py
@@ -3,10 +3,15 @@ from app import app
 from app import script
 from flask import render_template, send_from_directory
 
-@app.route('/')
-@app.route('/result')
-def index():
-    response = script.do()
+@app.route('/<id>')
+@app.route('/result/<id>')
+def index(id):
+    response = script.do(id)
     content = response[0].decode().split("SplittingSentense")
     url = response[1]
     return render_template('index.html', title=content[0], content = content[1], url = url)
+
+@app.route('/favicon.ico')
+def favicon():
+    return ""
+    
