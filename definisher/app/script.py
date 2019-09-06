27ab97f53bb3a9a286945becfd48e23be94c2f30
Bialaz <igor.bialas@gmail.com>
2019-09-06 22:46:58 +0200

    jd

--- definisher/app/script.py
+++ definisher/app/script.py
@@ -1,15 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-def do():
+def do(category):
     while True:
-        r = requests.get('https://en.wikipedia.org/wiki/Special:Random')
-        soup = BeautifulSoup(r.text, 'html.parser')
-        p = soup.p
-        soupP = BeautifulSoup(p.text, 'html.parser')
-        if p.text.strip():
-            content = soup.h1.text + "SplittingSentense" + soupP.text
+        if category:
+            r = requests.get("https://en.wikipedia.org/wiki/Special:RandomInCategory/" + str(category))
+            rightUrl = r.url.replace("Category:", '')
+            rightUrl = rightUrl.replace("Talk:", '')
+            if r.url!=rightUrl:
+                r = requests.get(rightUrl)
+
+        else:
+            r = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+
+        soup = BeautifulSoup(r.text, "html.parser")
+        print(soup.p.text)
+
+        if soup.p.text.strip() and soup.p.text.strip()!="Other reasons this message may be displayed:":
+            content = soup.h1.text + "SplittingSentense" + soup.p.text
+
             for x in list(range(0,20)):
                 check = '[{0}]'.format(str(x))
-                content = content.replace(check, '')
+                content = content.replace(check, "")
+
             return str(content).encode(), r.url
