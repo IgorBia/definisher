c9b6f288bf402eb15e7b0e884ab29729806307b0
GummyBear <dawid1siekanski@gmail.com>
2019-10-03 20:52:32 +0200

    center align added

--- definisher/app/routes.py
+++ definisher/app/routes.py
@@ -7,7 +7,7 @@ from flask import render_template, send_from_directory
 @app.route('/result/<id>')
 def index(id):
     response = script.do(id)
-    return render_template('index.html', title=response[0], content = response[1], url = response[2])
+    return render_template('state1.html', title=response[0], content = response[1], url = response[2])
 
 @app.route('/favicon.ico')
 def favicon():
