50e799ccc333a854b3003e0b21a03269210dfc51
Bialaz <igor.bialas@gmail.com>
2020-04-19 00:35:20 +0200

    Does stuff at "/" route

--- definisher/app/routes.py
+++ definisher/app/routes.py
@@ -3,12 +3,18 @@ from app import app
 from app import script
 from flask import render_template, send_from_directory
 
-@app.route('/<id>')
+@app.route('/')
+def home():
+    response = script.do()
+    return render_template('state1.html', title=response[0], content = response[1], url = response[2], id = id)
+
 @app.route('/result/<id>')
 def index(id):
     response = script.do(id)
-    return render_template('state1.html', title=response[0], content = response[1], url = response[2])
+    return render_template('state1.html', title=response[0], content = response[1], url = response[2], id = id)
 
 @app.route('/favicon.ico')
+@app.route('/result/favicon.ico')
 def favicon():
-    return ""
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
